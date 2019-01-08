d4d61c8dc5b2272b226e1679a5633ab5e2752b22
Windesson Silva Almeida <windesson.silvaalmeida@boeing.com>
2019-01-08 18:13:56 -0500

    Added comic book and artist model

--- TreeHouseComicBooksGallery/Controllers/ComicBookController.cs
+++ TreeHouseComicBooksGallery/Controllers/ComicBookController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace ComicBooksGallery.Controllers
+namespace TreeHouseComicBooksGallery.Controllers
 {
-    public class ComicBooksController: Controller
+    public class ComicBookController: Controller
     {
         public ActionResult Index()
         {
---
+++ TreeHouseComicBooksGallery/Models/ComicBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreeHouseComicBooksGallery.Models
+{
+    public class ComicBook
+    {
+        public bool Favorite { get; set; }
+
+        public int Id { get; set; }
+
+        public string SeriesTitle { get; set; }
+
+        public int IssueNumber { get; set; }
+
+        public string Description { get; set; }
+
+        public Artist[] Artists { get; set; }
+
+    }
+}
