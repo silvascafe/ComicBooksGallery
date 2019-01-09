b8f0d6368ca9c23118648b6b4b9c54f2d2a70a0f
Windesson Silva Almeida <windesson.silvaalmeida@boeing.com>
2019-01-09 07:36:59 -0500

    Add HttpNotFound handler to the Comic Book Controller

---
+++ TreeHouseComicBooksGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using TreeHouseComicBooksGallery.Data;
+using TreeHouseComicBooksGallery.Models;
+
+namespace TreeHouseComicBooksGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        private ComicBookRepository _comicBookRepository;
+
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)  return HttpNotFound();
+
+            var comicBook = _comicBookRepository.GetComicBook((int) id);
+
+            if (comicBook == null) return HttpNotFound();
+
+            return View(comicBook);
+        }
+    }
+}
