1a42746fa262004e09432843d24efc8172820b1b
Windesson Silva Almeida <windesson.silvaalmeida@boeing.com>
2019-01-08 20:01:55 -0500

    Added comic book repository

--- TreeHouseComicBooksGallery/Controllers/ComicBookController.cs
+++ TreeHouseComicBooksGallery/Controllers/ComicBookController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using TreeHouseComicBooksGallery.Data;
 using TreeHouseComicBooksGallery.Models;
 
 namespace TreeHouseComicBooksGallery.Controllers
@@ -10,28 +13,16 @@ namespace TreeHouseComicBooksGallery.Controllers
             return View();
         }
 
-        public ActionResult Detail()
+       
+        public ActionResult Detail(int id)
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of" +
-                                  " Doctor Octopus' life and his one, last, great act of revenge! " +
-                                  "Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                   new Artist(){Name = "Dan Slott"       , Role = "Script" },
-                   new Artist(){Name = "Humberto Ramos"  , Role = "Pencils"  },
-                   new Artist(){Name = "Victor Olazab"   , Role = "Inks"     },
-                   new Artist(){Name = "Edgar Delgado"   , Role = "Colors"   },
-                   new Artist(){Name = "Chris Eliopoulos", Role = "Letters"  },
-                }
+            var comicBookRepository = new ComicBookRepository();
+            var comicBook = comicBookRepository.GetComicBook(id);
 
-             };
+            if(comicBook != null)
+              return View(comicBookRepository.GetComicBook(id));
 
-            return View(comicBook);
-
+            return Redirect("/");
         }
     }
 }
